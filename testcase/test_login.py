38bbe5764903f8201f80d885ac45be554d30152a
hanshoukai <hankai1202@163.com>
2018-04-22 12:26:39 +0800

    se_auto_test_frame

---
+++ testcase/test_login.py
@@ -0,0 +1,28 @@
+# /usr/bin/env python
+# coding=utf-8
+
+import unittest
+from selenium import webdriver
+from se_automation.PO.login_page import LoginPage
+import time
+
+class TestBaiduLogin(unittest.TestCase):
+    """UI自动化登录"""
+    def setUp(self):
+        self.url = "http://www.baidu.com"
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(20)
+        # self.verificationErrors = []
+
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.quit()
+        # self.assertEqual([],self.verificationErrors)
+
+    def test_login(self):
+        """百度登录"""
+        sp = LoginPage(self.driver)
+        sp.open(self.url)
+        sp.click_link()
+        sp.run_case("hanshoukai","123321")
+        self.assertEqual(sp.get_username(),"hanshoukai",msg="验证失败！")
